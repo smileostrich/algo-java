c1958aeb96664f133797688df24f3951a541b9cb
ilminmoon <ian.ilminmoon@gmail.com>
2021-09-17 20:53:39 +0900

    boj 1000

---
+++ src/boj/basic/basic_1000.java
@@ -0,0 +1,17 @@
+package boj.basic;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class basic_1000 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a+b);
+    }
+}
