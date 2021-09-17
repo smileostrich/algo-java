437bdce4b842c916cfeb9b20e153a27addc9ab27
ilminmoon <ian.ilminmoon@gmail.com>
2021-09-17 21:30:38 +0900

    boj 1330

--- src/boj/basic/basic_1330.java
+++ src/boj/basic/basic_1330.java
@@ -2,10 +2,24 @@ package boj.basic;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class basic_1330 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader()
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        Integer i1 = Integer.parseInt(str[0]);
+        Integer i2 = Integer.parseInt(str[1]);
+        if (i1 > i2) {
+            System.out.println(">");
+        }
+        else if (i1 < i2) {
+            System.out.println("<");
+        }
+        else {
+            System.out.println("==");
+        }
+
 
     }
 }
