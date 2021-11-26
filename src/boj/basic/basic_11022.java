8b7b5e96ee4c651f095bf7da1d7d6c9a6e17c088
ilminmoon <ian.ilminmoon@gmail.com>
2021-11-26 17:16:58 +0900

    boj 11022

---
+++ src/boj/basic/basic_11022.java
@@ -0,0 +1,19 @@
+package boj.basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class basic_11022 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = Integer.parseInt(br.readLine());
+        for (int i=1; i<=cnt; ++i) {
+            String[] s = br.readLine().split(" ");
+            Integer i1 = Integer.parseInt(s[0]);
+            Integer i2 = Integer.parseInt(s[1]);
+            int sum = i1+i2;
+            System.out.printf("Case #%d: %d + %d = %d%n", i, i1,i2,sum);
+        }
+    }
+}
